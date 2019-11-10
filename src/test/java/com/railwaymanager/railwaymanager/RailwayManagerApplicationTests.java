17e95df936143fad3ed6b317b141cf2cab0cc5d1
lijiwen <lijiwen@guazi.com>
2019-11-10 23:48:03 +0800

    567

---
+++ src/test/java/com/railwaymanager/railwaymanager/RailwayManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.railwaymanager.railwaymanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RailwayManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
