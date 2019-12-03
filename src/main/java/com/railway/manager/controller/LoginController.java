e61485af3e031b4b62a85d79184230bf9acdfe4c
lijiwen <lijiwen@guazi.com>
2019-12-03 14:29:42 +0800

    去掉登陆的验证码设置

--- src/main/java/com/railway/manager/controller/LoginController.java
+++ src/main/java/com/railway/manager/controller/LoginController.java
@@ -21,7 +21,7 @@ import java.util.Map;
  * @author: lijiwen
  * @create: 2019-09-15 15:48
  **/
-@Api(tags = "用户注册及登录相关接口",value = "用户注册及登录相关接口")
+@Api(tags = "用户注册及登录相关接口", value = "用户注册及登录相关接口")
 @Controller
 @RequestMapping("/api")
 public class LoginController {
@@ -32,63 +32,66 @@ public class LoginController {
     @Resource
     private VeriCodeService veriCodeService;
 
-    @GetMapping ("/login")
+    @GetMapping("/login")
     @ResponseBody
     @ApiOperation(value = "用户登录", notes = "用户登录")
-    public Map<String,Object> login(@RequestParam String userName, @RequestParam String password, @RequestParam String imageCode, @RequestParam String userTaskId) {
+    public Map<String, Object> login(@RequestParam String userName,
+                                     @RequestParam String password,
+                                     @RequestParam(required = false) String imageCode,
+                                     @RequestParam(required = false) String userTaskId) {
 
         //检查验证码
-        Map<String,Object> resultMap = new HashMap<String,Object>();
+        Map<String, Object> resultMap = new HashMap<String, Object>();
 
-        if(StringUtils.isBlank(imageCode)) {
-            resultMap.put("code","501");
-            resultMap.put("description","请输入验证码");
-            return resultMap;
-        }
-        if(StringUtils.isBlank(userTaskId)) {
-            resultMap.put("code","502");
-            resultMap.put("description","唯一标识错误");
-            return resultMap;
-        }
-
-        //验证不通过
-        if(!veriCodeService.verifyCode(imageCode.trim(), userTaskId.trim())) {
-            resultMap.put("code","503");
-            resultMap.put("description","验证码错误");
-            return resultMap;
-        }
+//        if (StringUtils.isBlank(imageCode)) {
+//            resultMap.put("code", "501");
+//            resultMap.put("description", "请输入验证码");
+//            return resultMap;
+//        }
+//        if (StringUtils.isBlank(userTaskId)) {
+//            resultMap.put("code", "502");
+//            resultMap.put("description", "唯一标识错误");
+//            return resultMap;
+//        }
+//
+//        //验证不通过
+//        if (!veriCodeService.verifyCode(imageCode.trim(), userTaskId.trim())) {
+//            resultMap.put("code", "503");
+//            resultMap.put("description", "验证码错误");
+//            return resultMap;
+//        }
 
         //核对参数
-        if(StringUtils.isBlank(userName)) {
-            resultMap.put("code","504");
-            resultMap.put("description","请输入用户名");
+        if (StringUtils.isBlank(userName)) {
+            resultMap.put("code", "504");
+            resultMap.put("description", "请输入用户名");
             return resultMap;
         }
-        if(StringUtils.isBlank(password)) {
-            resultMap.put("code","505");
-            resultMap.put("description","请输入密码");
+        if (StringUtils.isBlank(password)) {
+            resultMap.put("code", "505");
+            resultMap.put("description", "请输入密码");
             return resultMap;
         }
 
         //检查用户
-        Map<String,Object> paramMap = new HashMap<String,Object>();
+        Map<String, Object> paramMap = new HashMap<String, Object>();
         paramMap.put("userNameEqual", userName);
         paramMap.put("passwordEqual", password);
         List<User> userList = userService.getList(paramMap);
 
-        if(userList==null || userList.size()<1) {
-            resultMap.put("code","506");
-            resultMap.put("description","用户名或密码错误");
+        if (userList == null || userList.size() < 1) {
+            resultMap.put("code", "506");
+            resultMap.put("description", "用户名或密码错误");
             return resultMap;
-        } else if(userList.size() > 1) {
-            resultMap.put("code","507");
-            resultMap.put("description","存在多条用户信息，请及时联系管理员");
+        } else if (userList.size() > 1) {
+            resultMap.put("code", "507");
+            resultMap.put("description", "存在多条用户信息，请及时联系管理员");
             return resultMap;
         }
 
-        resultMap.put("code","200");
-        resultMap.put("description","登录成功");
-        resultMap.put("user",userList.get(0));
+        resultMap.put("code", "200");
+        resultMap.put("description", "登录成功");
+        resultMap.put("user", userList.get(0));
 
         //清除缓存中验证码
         veriCodeService.delCacheCode(imageCode.trim(), userTaskId.trim());
@@ -99,33 +102,33 @@ public class LoginController {
     @PostMapping("/registerUser")
     @ResponseBody
     @ApiOperation(value = "用户注册", notes = "用户注册")
-    public Map<String,Object> registerUser(@RequestParam String userName, @RequestParam String password, @RequestParam String pwdConfirm) {
+    public Map<String, Object> registerUser(@RequestParam String userName, @RequestParam String password, @RequestParam String pwdConfirm) {
 
-        Map<String,Object> resultMap = new HashMap<String,Object>();
+        Map<String, Object> resultMap = new HashMap<String, Object>();
 
-        if(StringUtils.isBlank(userName)) {
+        if (StringUtils.isBlank(userName)) {
 
-            resultMap.put("code","501");
-            resultMap.put("description","用户名不能为空");
+            resultMap.put("code", "501");
+            resultMap.put("description", "用户名不能为空");
             return resultMap;
         }
 
         //检查用户名是否唯一
-        if(!userService.checkUserName(userName)) {
-            resultMap.put("code","502");
-            resultMap.put("description","用户名已被注册");
+        if (!userService.checkUserName(userName)) {
+            resultMap.put("code", "502");
+            resultMap.put("description", "用户名已被注册");
             return resultMap;
         }
 
         //检查两次输入密码是否一致
-        if(StringUtils.isBlank(password)) {
-            resultMap.put("code","503");
-            resultMap.put("description","密码不能为空");
+        if (StringUtils.isBlank(password)) {
+            resultMap.put("code", "503");
+            resultMap.put("description", "密码不能为空");
             return resultMap;
         }
-        if(!password.trim().equals(pwdConfirm)) {
-            resultMap.put("code","504");
-            resultMap.put("description","确认密码与密码不一致");
+        if (!password.trim().equals(pwdConfirm)) {
+            resultMap.put("code", "504");
+            resultMap.put("description", "确认密码与密码不一致");
             return resultMap;
         }
 
@@ -140,20 +143,20 @@ public class LoginController {
         try {
             int num = userService.add(user);
 
-            if(num > 0) {
-                resultMap.put("code","200");
-                resultMap.put("description","注册成功");
-                resultMap.put("user",user);
+            if (num > 0) {
+                resultMap.put("code", "200");
+                resultMap.put("description", "注册成功");
+                resultMap.put("user", user);
                 return resultMap;
             } else {
-                resultMap.put("code","505");
-                resultMap.put("description","注册失败");
+                resultMap.put("code", "505");
+                resultMap.put("description", "注册失败");
                 return resultMap;
             }
         } catch (Exception e) {
             e.printStackTrace();
-            resultMap.put("code","506");
-            resultMap.put("description","注册失败");
+            resultMap.put("code", "506");
+            resultMap.put("description", "注册失败");
             return resultMap;
         }
     }
