bc21b8ac97279dba1313ea991a484cc10f8fe501
chenglincheng <1404163360@qq.com>
2020-01-04 18:56:43 +0800

    修改权限接口xml

--- src/main/java/com/railway/manager/vo/PermissionVo.java
+++ src/main/java/com/railway/manager/vo/PermissionVo.java
@@ -1,4 +1,4 @@
-package com.railway.manager.model;
+package com.railway.manager.vo;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
@@ -9,14 +9,14 @@ import java.util.Date;
 
 /**
  * @program: railway_manager
- * @description: 权限
+ * @description: 权限vo
  * @author: chenglin
  * @create: 2020-01-04 16:00
  **/
 @Data
 @Accessors(chain = true)
-@ApiModel("权限信息")
-public class Permission {
+@ApiModel("权限信息vo")
+public class PermissionVo {
 
     @ApiModelProperty("id")
     private Integer id;
@@ -26,31 +26,22 @@ public class Permission {
     private String name;
     @ApiModelProperty("路径")
     private String url;
-    @ApiModelProperty("组件")
-    private String component;
-    @ApiModelProperty("组件名字")
-    private String componentName;
     @ApiModelProperty("0目录 1菜单 2按钮权限")
     private Integer menuTypeCode;
+    @ApiModelProperty("权限菜单类型")
+    private String menuTypeName;
     @ApiModelProperty("权限标识")
     private String perms;
-    @ApiModelProperty("菜单排序")
+    @ApiModelProperty("菜单顺序")
     private Integer sortNo;
-    @ApiModelProperty("菜单图标")
-    private String icon;
     @ApiModelProperty("是否叶子节点: 1:是 0:不是")
     private Integer isLeaf;
+    @ApiModelProperty("是否叶子节点: 1:是 0:不是")
+    private String isLeafVal;
     @ApiModelProperty("描述")
     private String description;
     @ApiModelProperty("状态（1启用 0不启用）")
     private Integer status;
-
-    @ApiModelProperty("创建人")
-    private String createUser;
-    @ApiModelProperty("创建时间")
-    private Date createdTime;
-    @ApiModelProperty("修改人")
-    private String updateUser;
-    @ApiModelProperty("修改时间")
-    private Date updatedTime;
+    @ApiModelProperty("状态（1启用 0不启用）")
+    private Integer statusName;
 }
